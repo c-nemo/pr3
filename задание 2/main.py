36b0da1dccc9717108496854d9a21debf9b5ea07
Aleksandra Kuroedova <sasha.kuroedova@gmail.com>
2022-03-20 17:37:52 +0300

    Commit2

---
+++ задание 2/main.py
@@ -0,0 +1,14 @@
+import module_1
+
+module_1.print_hello()
+
+import os
+# меняем местами файлы
+os.rename("module_1.py", "module_3.py")
+os.rename("module_2.py", "module_1.py")
+os.rename("module_3.py", "module_2.py")
+
+import module_1
+
+module_1.print_hello()
+
